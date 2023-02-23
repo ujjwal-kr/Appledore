2553ca02e3e1fc6c74feaa66d0e4723f20dd9c1e
Ujjwal <epicujjwal@protonmail.com>
2023-02-23 15:06:17 +0000

    readme

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-// Uncomment this block to pass the first stage
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                println!("accepted new connection, {:#?}", _stream);
             }
             Err(e) => {
                 println!("error: {}", e);
