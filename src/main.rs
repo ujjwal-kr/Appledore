0cf6fe553d1f7d188ba496580556a212399a1c02
Ujjwal <epicujjwal@protonmail.com>
2023-02-25 04:06:13 +0000

    add ping command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection, {:#?}", _stream);
+            Ok(s) => {
+                handle_connection(s)
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    stream.read(&mut buf).unwrap();
+    println!("{:?}", buf);
+    stream.write("+PONG\r\n".as_bytes()).unwrap();
 }
